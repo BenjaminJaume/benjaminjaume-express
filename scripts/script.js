70194b6f088d0db37e67ebcfe33f348377266b6b
BenjaminJaume <benjamin.jaume@gmail.com>
2019-10-11 21:43:51 +1100

    Creating new pages

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,6 @@ $(function() {
   $('[data-toggle="popover"]').popover();
 });
 
-$('[data-toggle=popover]').popover({ html: true });
-
-function copyPhoneNumber() {
-  var text = '+61432586316';
-
-  text.select();
-  text.setSelectionRange(0, 99999); /*For mobile devices*/
-
-  /* Copy the text inside the text field */
-  document.execCommand('copy');
-
-  /* Alert the copied text */
-  alert('Copied the text: ' + text.value);
-}
+$('[data-toggle=popover]').popover({
+  html: true
+});
