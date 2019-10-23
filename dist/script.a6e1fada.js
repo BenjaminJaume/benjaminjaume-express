c7719545b36b9bc6dce70c74e20e62b761b0e352
BenjaminJaume <benjamin.jaume@gmail.com>
2019-10-23 21:29:03 +1100

    Fix portfolio navbar

--- dist/script.a6e1fada.js
+++ dist/script.a6e1fada.js
@@ -123,7 +123,11 @@ $(function () {
 });
 $('[data-toggle=popover]').popover({
   html: true
-});
+}); // To implement later on with a server
+// fetch('./downloads.txt')
+//   .then(response => response.text())
+//   .then(text => console.log(text));
+// // outputs the content of the text file
 },{}],"../../../Users/Benjamin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -152,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59069" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63514" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,9 @@ $(function() {
 $('[data-toggle=popover]').popover({
   html: true
 });
+
+// To implement later on with a server
+// fetch('./downloads.txt')
+//   .then(response => response.text())
+//   .then(text => console.log(text));
+// // outputs the content of the text file
