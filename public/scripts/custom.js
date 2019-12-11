e907d485f9c779e7bbc5adb6cc7f8127a0a8d0bd
BenjaminJaume <benjamin.jaume@gmail.com>
2019-12-11 14:10:43 -0600

    Implement ExpressJS

---
+++ public/scripts/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  // get current URL path and assign 'active' class
+  // var pathname = window.location.pathname;
+  // $('.navbar-nav > li > a[href="' + pathname + '"]')
+  //   .parent()
+  //   .addClass("active");
+});
+
+// $(function() {
+//   $('[data-toggle="popover"]').popover();
+// });
+
+// $("[data-toggle=popover]").popover({
+//   html: true
+// });
+
+// To implement later on with a server
+// fetch('./downloads.txt')
+//   .then(response => response.text())
+//   .then(text => console.log(text));
+// // outputs the content of the text file
--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, './index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname, "./index.html");
 });
 
-app.get('/admin', function(req, res) {
-  res.sendFile(path.join(__dirname, 'admin.html'));
-});
+app.use(express.static("public"));
+app.use("/portfolio", express.static("public"));
+app.use("/images", express.static("public"));
+app.use("/scripts", express.static("public"));
 
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/public', express.static('./public'));
-
-app.listen(3000, () =>
-  console.log(`Example app listening on http://localhost:3000`)
-);
+app.listen(3000, () => console.log(`App listening on http://localhost:3000`));
