368ee096e9a0c1d268d999679e5240d002957672
BenjaminJaume <benjamin.jaume@gmail.com>
2019-12-13 15:43:54 -0600

    test

--- test.php
+++ test.php
@@ -1,7 +1,3 @@
 <?php
-    $db_connection = pg_connect("host='ec2-174-129-255-57.compute-1.amazonaws.com' port='5432' dbname='d9i01p9llna461' user='faaotfmmmurdll' password='f2ced1ed29bf887139acaf764e1331cc6bbc6bafdb77e93dfff45b03618a91cc');
-
-    $result = pg_query($db_connection, 'SELECT * FROM users');
-
-    echo $result;
+    echo 'Bonjour'
 ?>
